51450136f8363bf2be54ede7ea75eb0f35ce7eab
Jao-Couto <73503848+Jao-Couto@users.noreply.github.com>
2022-08-20 12:55:07 -0300

    .bat para executar tudo

---
+++ node.bat
@@ -0,0 +1,122 @@
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\acidentes-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\adocao-animais-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\ambientes-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\animais-abandonados-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\animais-perdidos-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\animais-sinantropicos-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\anticorrupcao-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\api-gateway"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\cidadao-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\cidade-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\diarista-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\facilities-inspection"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\feira-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\foco-escorpiao-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\fountains-monuments"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\gestores-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\idealizacaoComercial-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\insetos-roedores-caramujos-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\leishmaniose-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\limpeza-piscinas-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\limpeza-terreno-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\locais-uteis-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\maus-tratos-animais-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\menu-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\menucidade-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\noticias-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\ofertas-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\parques-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\paviment"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\praca-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\programacao-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\propostas-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\public-area-adoption"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\public-roads"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\radar-dengue-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\rurais-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\street-lighting"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\submenu-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\sugestoes-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\tumulos-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\useful-contacts"
+start npm start
