1afa16c9fb9e5e86028fb8e33c9da9db6fa9d9cb
Joao Couto <joao.vitor1229@hotmail.com>
2022-12-03 13:15:24 -0300

    executar basico

---
+++ essencial.bat
@@ -0,0 +1,20 @@
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\api-gateway"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\cidadao-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\cidade-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\menu-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\menucidade-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\praca-service"
+start npm start
+
+cd "C:\Users\joaov\Documents\GitHub\API_UnespSCity\packages\submenu-service"
+start npm start
